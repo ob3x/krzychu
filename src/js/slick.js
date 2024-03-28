9e1a79b57be89cf458382a0c291f789e7f534f28
Szymon <ob3x.biznes@gmail.com>
2024-03-28 13:13:52 +0100

    bigUpdate

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,14 +1,35 @@
 $(document).ready(function () {
 	$('.news-box').slick({
-		slidesToShow: 2,
+		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 2500,
+		autoplaySpeed: 1000,
 		arrows: false,
 		mobileFirst: true,
 		responsive: [
 			{
-				breakpoint: 700,
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1000,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1500,
 				settings: {
 					slidesToShow: 5,
 					slidesToScroll: 1,
